b82952e1710f69c6b1755c65d30acf505b4d1ba5
hjs6877 <hjs6877@gmail.com>
2022-08-17 13:50:33 +0900

    Remove unnecessary files

--- src/main/java/com/codestates/helper/email/EmailSendable.java
+++
@@ -1,8 +0,0 @@
-package com.codestates.helper.email;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface EmailSendable {
-    void send(String message) throws InterruptedException;
-}
