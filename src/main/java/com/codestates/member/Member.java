251b117d639df44e4ee694b1de3e0ffd23a7f222
hjs6877 <hjs6877@gmail.com>
2022-08-31 18:10:13 +0900

    Modify password column length

--- src/main/java/com/codestates/member/Member.java
+++ src/main/java/com/codestates/member/Member.java
@@ -22,7 +22,7 @@ public class Member extends Auditable {
     @Column(nullable = false, updatable = false, unique = true)
     private String email;
 
-    @Column(length = 20, nullable = false)
+    @Column(length = 100, nullable = false)
     private String password;
 
     @Enumerated(value = EnumType.STRING)
