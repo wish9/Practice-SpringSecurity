519ec209d392c23d7a9f0ec54e764f6145833f05
wish9 <wjwee9@gmail.com>
2023-03-15 00:48:32 +0900

    new branch test

--- src/main/java/com/codestates/HelloSpringSecurityApplication.java
+++ src/main/java/com/codestates/HelloSpringSecurityApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class HelloSpringSecurityApplication {
+public class HelloSpringSecurityApplication { // new branch test
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringSecurityApplication.class, args);
