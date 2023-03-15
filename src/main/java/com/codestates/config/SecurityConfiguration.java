d42a8f81c5130657a1d68f641db5623e8157b53e
wish9 <wjwee9@gmail.com>
2023-03-15 10:37:43 +0900

    커스텀 로그인 페이지 지정하기 (Spring Security, 인메모리 사용, SSR방식)

--- src/main/java/com/codestates/config/SecurityConfiguration.java
+++ src/main/java/com/codestates/config/SecurityConfiguration.java
@@ -2,13 +2,32 @@ package com.codestates.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // Spring Security Configuration 적용 = 인증 방식, 웹 페이지에 대한 접근 권한 설정 가능
 public class SecurityConfiguration {
+
+    // HttpSecurity클래스 = HTTP 요청에 대한 보안 설정을 구성하기 위한 핵심 클래스
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()                 // CSRF 공격에 대한 Spring Security에 대한 설정을 비활성화 (로컬 환경에서 테스트하기 때문)
+                .formLogin()                      // 기본적인 인증 방법을 폼 로그인 방식으로 지정
+                .loginPage("/auths/login-form")   // 만들어 둔 커스텀 로그인 페이지를 사용하도록 설정
+                .loginProcessingUrl("/process_login")    // 로그인 인증 요청을 수행할 요청 URL을 지정
+                .failureUrl("/auths/login-form?error")   // 로그인 실패시 뜨게 할 화면 지정
+                .and()                                   // Spring Security 보안 설정을 메서드 체인 형태로 구성
+                .authorizeHttpRequests()                     // 클라이언트의 요청이 들어오면 접근 권한을 확인하겠다고 정의
+                .anyRequest()                            // 클라이언트의 모든 요청에 대해
+                .permitAll();                            // 접근을 허용
+
+        return http.build();
+    }
     @Bean
     public UserDetailsManager userDetailsService() {
 
